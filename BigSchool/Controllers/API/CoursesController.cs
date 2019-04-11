772c8dde46105b4658e3fda93f3ef1d89ed02f2a
tranvanben <trandangben1997@gmail.com>
2019-04-11 15:00:47 +0700

    Add project files.

---
+++ BigSchool/Controllers/API/CoursesController.cs
@@ -0,0 +1,34 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BigSchool.Controllers.API
+{
+    public class CoursesController : ApiController
+    {
+        public ApplicationDbContext dbContext { get; set; }
+        public CoursesController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var UserId = User.Identity.GetUserId();
+            var course = dbContext.Courses.Single(c => c.Id == id && c.LecturerId == UserId);
+            if (course.IsCanceled)
+            {
+                return NotFound();
+            }
+            course.IsCanceled = true;
+            dbContext.SaveChanges();
+            return Ok();
+
+        }
+    }
+}
